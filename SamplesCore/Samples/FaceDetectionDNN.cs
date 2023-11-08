a93ee7f37efdc76bcd71076701ecf713e3f88f87
Trần Văn Hảo <haovtit@gmail.com>
2023-11-08 22:13:09 +0700

    Sample test

--- SamplesCore/Samples/FaceDetectionDNN.cs
+++ SamplesCore/Samples/FaceDetectionDNN.cs
@@ -1,6 +1,9 @@
 ﻿using OpenCvSharp;
 using OpenCvSharp.Dnn;
 using SampleBase;
+using System.IO;
+using OpenCvSharp.ML;
+using OpenCvSharp.Face;
 
 namespace SamplesCore
 {
@@ -11,19 +14,31 @@ namespace SamplesCore
     /// </summary>
     internal class FaceDetectionDNN : ConsoleTestBase
     {
-        const string configFile = "deploy.prototxt";
-        const string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
-        const string image = "faces.jpg";
+        const string configFile = "Metadata\\deploy.prototxt";
+        const string faceModel = "Metadata\\res10_300x300_ssd_iter_140000_fp16.caffemodel";
+        const string image1 = "Metadata\\faces.jpg";
+        //const string image1 = "Metadata\\others.jpg";
 
         public override void RunTest()
         {
+            string currentDir = Directory.GetCurrentDirectory();
+            string combined = Path.Combine(currentDir, image1);
+            if (!File.Exists(combined))
+            {
+                throw new FileNotFoundException();
+            }
+
             // Read sample image
-            using var frame = Cv2.ImRead(image);
+            using var frame = Cv2.ImRead(image1);
             int frameHeight = frame.Rows;
             int frameWidth = frame.Cols;
             using var faceNet = CvDnn.ReadNetFromCaffe(configFile, faceModel);
             using var blob = CvDnn.BlobFromImage(frame, 1.0, new Size(300, 300), new Scalar(104, 117, 123), false, false);
             faceNet.SetInput(blob, "data");
+
+
+            var recognizer = LBPHFaceRecognizer.Create(frameWidth, frameHeight);
+            recognizer.Read("Metadata\\trained_recognizer.xml");
 
             using var detection = faceNet.Forward("detection_out");
             using var detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F,
@@ -40,8 +55,27 @@ namespace SamplesCore
                     int y2 = (int) (detectionMat.At<float>(i, 6) * frameHeight);
 
                     Cv2.Rectangle(frame, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0), 2, LineTypes.Link4);
+
+                    int label;
+                    double confid;
+                    recognizer.Predict(detectionMat, out label, out confid);
+                    if (label > 0)
+                    {
+                        Cv2.PutText(frame, $"Person: {label}", new Point(x1, y1), HersheyFonts.HersheySimplex, 1, new Scalar(0, 255, 0));
+                    }
+                    else
+                    {
+                        Cv2.PutText(frame, "Unknown", new Point(x1, y1), HersheyFonts.HersheySimplex, 1, new Scalar(0, 255, 0));
+                    }
+                  
                 }
             }
+
+
+  
+           
+
+
 
             Window.ShowImages(frame);
         }
